d9d9f0924c10798fad6560c1c9b9341092798e56
Youngmin Wang <ymw7067@gmail.com>
2017-05-11 09:14:23 +0000

    App created.

---
+++ ESL_UI_Sample/components/home/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+app.home = kendo.observable({
+    onShow: function() {},
+    afterShow: function() {}
+});
+app.localization.registerView('home');
+
+// START_CUSTOM_CODE_home
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_home
