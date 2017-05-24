e97856174b36a427cbc60a8b8b5edc1e248956da
Chang-Hoon Park <workdict@gmail.com>
2017-05-24 01:13:18 +0000

    Changes made by Chang-Hoon Park (workdict@gmail.com).

--- ESL_UI_Sample/components/viewType1/index.js
+++ ESL_UI_Sample/components/viewType1/index.js
@@ -1,149 +1,188 @@
 'use strict';
 
 app.viewType1 = kendo.observable({
-    _vtop: 0,
-    onShow: function() {},
-    afterShow: function() {}
+    screen: null,
+    model: null,
+    vtop: 0,
+
+    // event
+    onShow: function(e) {},
+    afterShow: function(e) {},
+    beforeHide: function(e) {},
+
+    // method
+    initScroll: function(e) {},
+    initTemplate: function(e) {},
+    initImages: function(e) {},
+    initDragdrop: function(e) {}
 });
 app.localization.registerView('viewType1');
+//------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // START_CUSTOM_CODE_viewType1
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
 // END_CUSTOM_CODE_viewType1
-(function(parent) {
+(function(view) {
     var
     /// start global model properties
 
-        processImage = function(img) {
+    processImage = function(img) {
+        if (!img) {
+            var empty1x1png = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVQI12NgYAAAAAMAASDVlMcAAAAASUVORK5CYII=';
+            img = 'data:image/png;base64,' + empty1x1png;
+        }
+        return img;
+    },
+    /// end global model properties
 
-            if (!img) {
-                var empty1x1png = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVQI12NgYAAAAAMAASDVlMcAAAAASUVORK5CYII=';
-                img = 'data:image/png;base64,' + empty1x1png;
-            }
-
-            return img;
-        },
-        /// end global model properties
-
-        viewType1Model = kendo.observable({
-            /// start add model functions
-            /// end add model functions
-
-        });
-
-    /// start form functions
-    /// end form functions
-    
-    parent.set('onInit', function _onInit(e) {
-        $("<div id='sticky_dummy' e-fixed='no'></div>").insertBefore($("#sticky_content", "#viewType1Screen"));
-
-      var scroller = e.view.scroller;
-      scroller.bind("scroll", function(e) {
-          var top = $("#sticky_dummy", "#viewType1Screen").offset().top;
-          var fixed = $("#sticky_dummy", "#viewType1Screen").attr("e-fixed");
-
-          if( top )
-            parent._vtop = top;
-
-          if( parent._vtop <= e.scrollTop  ) {
-            if( fixed == "no" ) {
-                // sticky_panel 로 옮긴다.
-                $("#sticky_target", "#viewType1Screen").append($("#sticky_content", "#viewType1Screen"));
-                $("#sticky_dummy", "#viewType1Screen").attr("e-fixed", "yes");
-                $("#sticky_panel", "#viewType1Screen").show();
-                $("#sticky_source", "#viewType1Screen").hide();
-            }
-          } else {
-            if( fixed == "yes" ) {
-                $("#sticky_dummy", "#viewType1Screen").after($("#sticky_content", "#viewType1Screen"));
-                $("#sticky_dummy", "#viewType1Screen").attr("e-fixed", "no");
-                $("#sticky_panel", "#viewType1Screen").hide();
-                $("#sticky_source", "#viewType1Screen").show();
-            }
-          }
-
-        //$("#info").html($("#sticky").offset().top + "; " + e.scrollTop + ", " + e.scrollLeft);
-      });
+    model = kendo.observable({
+        /// start add model functions
+        questions : [
+            { image_src: "resources/Picture1.png", word: "cool" },
+            { image_src: "resources/Picture2.png", word: "kind" },
+            { image_src: "resources/Picture3.png", word: "immature" },
+            { image_src: "resources/Picture4.png", word: "spoiled" },
+            { image_src: "resources/Picture5.png", word: "generous" },
+            { image_src: "resources/Picture6.png", word: "messy" },
+            { image_src: "resources/Picture7.png", word: "brave" },
+            { image_src: "resources/Picture8.png", word: "idiot" },
+            { image_src: "resources/Picture9.png", word: "genius" }
+        ]
+        /// end add model functions
     });
 
-    parent.set('onShow', function _onShow() {
-        var that = parent;
-        that.set('addFormData', {
+    //------------------------------------------------------------------------------------------------------------------------------------------------------------
+    /// start form functions
+    view.set('onInit', function(e) {
+        // 초기화 루틴
+        view.screen = $("#viewType1Screen")
+
+        view.initScroll(e);
+        view.initTemplate(e);
+        view.initImages(e);
+        view.initDragdrop(e);
+    });
+
+    view.set('onShow', function(e) {
+        view.set('addFormData', {
             /// start add form data init
             /// end add form data init
         });
         /// start add form show
-        /****************************************
-         *  Draggable
-         ****************************************/
-        $("img", ".productitem").each(function(i, el) {
-            var box_width = $(".product").width();
-            var box_height = $(".product").height();
-            $(el).load(function() {
+        
+        // 재방문 할때를 위해 초기화 한다.
+        view.vtop = 0;
+        e.view.scroller.scrollTo(0,0);
+        $(".en-draggable", view.screen).show();
+        $(".en-answer", view.screen).text("");
+    });
 
-                if (120 < $(el).height()) {
-                    $(el).css({  "width": "auto", "height": "120px" });
+    view.set('beforeHide', function(e) {
+        e.view.scroller.scrollTo(0,0);
+    });
+
+    view.set('model', model);
+    /// end form functions
+
+    //------------------------------------------------------------------------------------------------------------------------------------------------------------
+    view.set('initScroll', function(e) {
+        $('<div class="en-sticky-dummy" en-fixed="no"></div>').insertBefore($(".en-sticky-content", view.screen));
+
+        var scroller = e.view.scroller;
+        scroller.bind("scroll", function(e) {
+            var top = $(".en-sticky-dummy", view.screen).offset().top;
+            var fixed = $(".en-sticky-dummy", view.screen).attr("en-fixed");
+
+            if( top ) view.vtop = top;
+
+            if( view.vtop <= e.scrollTop  ) {
+                if( fixed == "no" ) {
+                    // sticky_panel 로 옮긴다.
+                    $(".en-sticky-target", view.screen).append($(".en-sticky-content", view.screen));
+                    $(".en-sticky-dummy", view.screen).attr("en-fixed", "yes");
+                    $(".en-sticky-panel", view.screen).show();
+                    $(".en-sticky-source", view.screen).hide();
+                }
+            } else {
+                if( fixed == "yes" ) {
+                    $(".en-sticky-dummy", view.screen).after($(".en-sticky-content", view.screen));
+                    $(".en-sticky-dummy", view.screen).attr("en-fixed", "no");
+                    $(".en-sticky-panel", view.screen).hide();
+                    $(".en-sticky-source", view.screen).show();
+                }
+            }
+        });
+    });
+
+    //------------------------------------------------------------------------------------------------------------------------------------------------------------
+    view.set('initTemplate', function (e) {
+        var template, result;
+
+        template = kendo.template($(".en-buttons-template").html());
+        var randomArr = new kendo.data.ObservableArray( shuffle( view.model.questions.slice(), { 'copy': true }) );
+
+        result = kendo.render(template, randomArr);
+        $(".en-sticky-content", view.screen).html(result);
+
+        template = kendo.template($(".en-list-template").html());
+        result = kendo.render(template, view.model.questions);
+        $(".en-list-view", view.screen).html(result);
+    });
+
+    //------------------------------------------------------------------------------------------------------------------------------------------------------------
+    view.set('initImages', function (e) {
+        $(".en-image-panel img", view.screen).each(function(i, el) {
+            $(el).load(function() {
+                if ($(el).height() > 120) {
+                    $(el).removeClass("en-fix-width");
+                    $(el).addClass("en-fix-height");
+                }
+                if ($(el).width() > 150) {
+                    $(el).removeClass("en-fix-height");
+                    $(el).addClass("en-fix-width");
                 }
             });
         });
-         
-        $(".draggable", "#viewType1Screen").kendoDraggable({
-            hint: function(elem) {
-                return elem.clone();
+    });
+
+    //------------------------------------------------------------------------------------------------------------------------------------------------------------
+    view.set('initDragdrop', function (e) {
+        $(".en-draggable", view.screen).kendoDraggable({
+            hint: function(el) {
+                return el.clone();
             },
             dragstart: function(e) {
-                e.currentTarget.addClass("drag");
+                e.currentTarget.addClass("en-drag");
             },
             dragend: function(e) {
-                e.currentTarget.removeClass("drag");
-                $(".droptarget", "#viewType1Screen").css("border-color", "#efefef").css("border-width", "1px");
+                e.currentTarget.removeClass("en-drag");
+                $(".en-droptarget", view.screen).removeClass("en-draggable-target");
             }
         });
 
-        $(".droptarget", "#viewType1Screen").kendoDropTarget({
+        $(".en-droptarget", view.screen).kendoDropTarget({
             dragenter: function(e) {
-                 e.dropTarget.css("border-color", "#000088").css("border-width", "2px");
+                 e.dropTarget.addClass("en-draggable-target");
             },
             dragleave: function(e) {
-                e.dropTarget.css("border-color", "#efefef");
+                e.dropTarget.removeClass("en-draggable-target");
             },
             drop: function(e) {
-                e.draggable.element.hide();
-                if (e.dropTarget.find(".answer").text() != "") {
-                    var text = e.dropTarget.find(".answer").text();
-                    $("button").each(function(i) {
-                        if ($(this).text() == text) {
-                            $(this).show();
-                        }
-                    });
-                }
-                e.dropTarget.find(".answer").text(e.draggable.element.text());
-                if( e.dropTarget.find(".answer").text() == e.dropTarget.find(".answer").attr("e-word") ) {
-                    e.dropTarget.find(".answer").removeClass("wrong"); 
-                } else {
-                    e.dropTarget.find(".answer").addClass("wrong");
-                } 
+                var word = e.draggable.element;
+                var answer = e.dropTarget.find(".en-answer");
 
-                e.draggable.element.removeClass("drag");
-                e.dropTarget.find(".items_delete").show();
+                if( word.text() != answer.attr("en-word") ) {
+                    e.preventDefault();
+                    return;
+                }
+
+                word.hide();
+                answer.text(word.text());
+                word.removeClass("en-drag");
             }
         });
-
-        $(".items_delete, .answer").click(function(e) {
-            var text = $(this).parent().find(".answer").text();
-            $("button").each(function(i) {
-                if ($(this).text() == text) {
-                    $(this).show();
-                }
-            });
-            $(this).parent().find(".items_delete").hide();
-            $(this).parent().find(".answer").text("");
-        });
-
-
-        /// end add form show
     });
-    parent.set('viewType1Model', viewType1Model);
+
 })(app.viewType1);
 
 // START_CUSTOM_CODE_viewType1Model
