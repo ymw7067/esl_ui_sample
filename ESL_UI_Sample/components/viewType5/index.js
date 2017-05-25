9a8e5667c41e747bee173508f0478d6f3f8aa71d
Chang-Hoon Park <workdict@gmail.com>
2017-05-25 06:34:31 +0000

    Changes made by Chang-Hoon Park (workdict@gmail.com).

--- ESL_UI_Sample/components/viewType5/index.js
+++ ESL_UI_Sample/components/viewType5/index.js
@@ -108,7 +108,7 @@ app.localization.registerView('viewType5');
                 }
                 $(".en-table-blank.selected", "#sticky-panel").text($(this).text());
                 $(".en-table-blank, .en-table-word", "#sticky-panel").removeClass("selected");
-                $(this).addClass("en-no-mark");
+                //$(this).addClass("en-no-mark");
             } else {
                 if ($(this).text() != $(".en-table-blank.selected", view.screen).attr("en-word")) {
                     alert("정답아님 !!")
@@ -116,7 +116,7 @@ app.localization.registerView('viewType5');
                 }
                 $(".en-table-blank.selected", view.screen).text($(this).text());
                 $(".en-table-blank, .en-table-word", view.screen).removeClass("selected");
-                $(this).addClass("en-no-mark");
+                //$(this).addClass("en-no-mark");
             }
         });
     });
@@ -174,82 +174,3 @@ app.localization.registerView('viewType5');
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
 
 // END_CUSTOM_CODE_viewType5
-
-/*
-(function(parent) {
-
-    parent.set('onInit', function _onInit(e) {
-        $("<div id='sticky_dummy' e-fixed='no'></div>").insertBefore($("#sticky_content", "#viewType5Screen"));
-
-      var scroller = e.view.scroller;
-      scroller.bind("scroll", function(e) {
-          var top = $("#sticky_dummy", "#viewType5Screen" ).offset().top;
-          var fixed = $("#sticky_dummy", "#viewType5Screen").attr("e-fixed");
-
-          if( top )
-            parent._vtop = top;
-
-          if( parent._vtop <= e.scrollTop - 60  ) {
-            if( fixed == "no" ) {
-                // sticky_panel 로 옮긴다.
-                $("#sticky_target", "#viewType5Screen").append($("#sticky_content", "#viewType5Screen"));
-                $("#sticky_dummy", "#viewType5Screen").attr("e-fixed", "yes");
-                $("#sticky_panel", "#viewType5Screen").show();
-                $("#sticky_source", "#viewType5Screen").hide();
-            }
-          } else {
-            if( fixed == "yes" ) {
-                $("#sticky_dummy", "#viewType5Screen").after($("#sticky_content", "#viewType5Screen"));
-                $("#sticky_dummy", "#viewType5Screen").attr("e-fixed", "no");
-                $("#sticky_panel", "#viewType5Screen").hide();
-                $("#sticky_source", "#viewType5Screen").show();
-            }
-          }
-
-        //$("#info").html($("#sticky").offset().top + "; " + e.scrollTop + ", " + e.scrollLeft);
-      });
-    });
-
-
-    parent.set('onShow', function _onShow(e) {
-        $(".draggable", "#viewType5Screen").kendoDraggable({
-            hint: function(elem) {
-                var clone = elem.clone();
-                clone.css("font-size", "40px");
-                return clone;
-            },
-            dragstart: function(e) {
-                e.currentTarget.addClass("drag");
-            },
-            dragend: function(e) {
-                e.currentTarget.removeClass("drag");
-                e.currentTarget.css("border-color", "#888888");
-            }
-        });
-
-        $(".droptarget", "#viewType5Screen").kendoDropTarget({
-            dragenter: function(e) {
-                 e.dropTarget.css("border-color", "#0000ff");
-                 e.dropTarget.next().addClass("dragover");
-            },
-            dragleave: function(e) {
-                e.dropTarget.css("border-color", "#888888");
-                e.dropTarget.next().removeClass("dragover");
-            },
-            drop: function(e) {
-                e.dropTarget.text(e.draggable.element.text());
-                if( e.dropTarget.text() == e.dropTarget.attr("e-word") )
-                    e.dropTarget.removeClass("wrong"); 
-                else
-                    e.dropTarget.addClass("wrong"); 
-
-                e.draggable.element.removeClass("drag");
-                e.dropTarget.css("border-color", "#888888");
-                e.dropTarget.next().removeClass("dragover");
-                //e.dropTarget.find(".items_delete").show();
-            }
-        });
-    });
-    
-})(app.viewType5);
-*/
