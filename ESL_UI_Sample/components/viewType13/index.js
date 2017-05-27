ea6375f1b6f153ad87ae158b56af535e591eccb9
Youngmin Wang <ymw7067@gmail.com>
2017-05-27 13:01:51 +0000

    Changes made by Youngmin Wang (ymw7067@gmail.com).

---
+++ ESL_UI_Sample/components/viewType13/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+app.viewType13 = kendo.observable({
+    onShow: function() {},
+    afterShow: function() {}
+});
+app.localization.registerView('viewType13');
+
+// START_CUSTOM_CODE_viewType13
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_viewType13
